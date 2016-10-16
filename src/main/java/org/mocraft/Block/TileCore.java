b6feee3965477bfdd2c811b99c9d91fa92691717
jackey8616 <jackey8616@gmail.com>
2016-10-16 21:47:57 +0800

    10.16 21:47 - Fixed.
    Fixed : fix missing implements of ICore interface in TileCore.

--- src/main/java/org/mocraft/Block/TileCore.java
+++ src/main/java/org/mocraft/Block/TileCore.java
@@ -21,11 +21,13 @@ public class TileCore extends TileEntity implements ICore {
     private String name;
     private int level;
     private ArrayList<UUID> members = new ArrayList<UUID>();
+    private int x, y, z;
 
     public TileCore() {
         this.isUsing = false;
         this.lord = new UUID(0L, 0L);
         this.name = "null";
+        this.x = this.y = this.z = 0;
     }
 
     @Override
@@ -41,6 +43,9 @@ public class TileCore extends TileEntity implements ICore {
         compound.setBoolean("Using", isUsing);
         compound.setString("Lord", lord.toString());
         compound.setString("Name", name);
+        compound.setInteger("X", x);
+        compound.setInteger("Y", y);
+        compound.setInteger("Z", z);
     }
 
     @Override
@@ -50,6 +55,9 @@ public class TileCore extends TileEntity implements ICore {
         this.isUsing = compound.getBoolean("Using");
         this.lord = UUID.fromString(compound.getString("Lord"));
         this.name = compound.getString("Name");
+        this.x = compound.getInteger("X");
+        this.x = compound.getInteger("Y");
+        this.x = compound.getInteger("Z");
     }
 
     @Override
@@ -114,4 +122,34 @@ public class TileCore extends TileEntity implements ICore {
 
     @Override
     public void addMembers(UUID player) { this.members.add(player); }
+
+    @Override
+    public void setX(int x) {
+
+    }
+
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public void setY(int y) {
+
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void setZ(int z) {
+
+    }
+
+    @Override
+    public int getZ() {
+        return 0;
+    }
 }
